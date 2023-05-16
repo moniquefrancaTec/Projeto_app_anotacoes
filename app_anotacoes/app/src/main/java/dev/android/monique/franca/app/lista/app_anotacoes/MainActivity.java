9a822ff4647e6492fc296042ef3186eb9bb8d628
moniquefrancaTec <monique.franca.tec@gmail.com>
2023-05-16 20:05:03 -0300

    Layout app

---
+++ app_anotacoes/app/src/main/java/dev/android/monique/franca/app/lista/app_anotacoes/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.android.monique.franca.app.lista.app_anotacoes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
